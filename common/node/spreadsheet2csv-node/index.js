3865656fa8b0d9ea156efe79aff3986e17401cad
Anton Stoychev <antitoxic@gmail.com>
2013-07-21 20:33:03 +0100

    Path and naming fixes for csv convertor

--- common/node/spreadsheet2csv-node/index.js
+++ common/node/spreadsheet2csv-node/index.js
@@ -1,13 +1,14 @@
+var path = require('path');
 var exec = require('child_process').exec;
 var logger = require('../logger')();
 
-var pathToConvertor = '../../../apps/spreadsheet2csv/spreadsheet2csv.php';
+var pathToConvertor = path.join(__dirname,'../../../apps/spreadsheet2csv/spreadsheet2csv.php');
 
 exports = module.exports = {
 	convert: function(inputPath, outputPath, callback) {
-		exec("php " + pathToConvertor + '-f '+pathToXls+' -o '+outputPath, function (error, stdout, stderr) {
+		exec("php " + pathToConvertor + ' -f '+inputPath+' -o '+outputPath, function (error, stdout, stderr) {
 			if (!(error == null && stdout == null && stderr == null)) {
-				logger.error('conversion error! From '+pathToXls+' to '+outputPath);
+				logger.error('conversion error! From '+inputPath+' to '+outputPath+', err:'+error+', '+stdout);
 			}
 			callback();
 		});
