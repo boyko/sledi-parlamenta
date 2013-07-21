283891a983d0f027192ed0b6a23f2b80a9ff8ad2
Anton Stoychev <antitoxic@gmail.com>
2013-07-21 20:30:10 +0100

    Add required headers for crawling parliament.bg

--- common/node/downloader/index.js
+++ common/node/downloader/index.js
@@ -2,16 +2,28 @@ var fs = require('fs');
 var request = require('request');
 var logger = require('../logger')();
 
+var headers = {
+	"User-Agent": "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.72 Safari/537.36",
+	"Accept-Encoding": "gzip,deflate",
+	"Accept-Language": "bg,en-GB;q=0.8,en;q=0.6",
+	"Cache-Control": "max-age=0"
+}
 exports = module.exports = {
 	get: function(url, callback) {
-		request(url, function (err, res, html) {
+		request({
+			"url": url,
+			"headers": headers
+		}, function (err, res, html) {
 			if (err || res.statusCode != 200) {
-				logger.error("Can't retrieve " + url);
+				logger.error("Can't retrieve " + url + ", "+res.statusCode + ", err: "+res.statusCode);
 			}
 			callback(html);
 		})
 	},
 	save: function(url, destination, callback) {
-		request(url).pipe(fs.createWriteStream(destination)).on("finish", callback);
+		request({
+			"url": url,
+			"headers": headers
+		}).pipe(fs.createWriteStream(destination)).on("finish", callback);
 	}
 }
