bb33039e1ae18f63d9c30d76a30720c4bd63f3b4
Anton Stoychev <antitoxic@gmail.com>
2013-08-07 08:15:32 +0100

    Extract brra stuff in a class

--- apps/brra.bg-inquirer/crawler.js
+++ apps/brra.bg-inquirer/crawler.js
@@ -1,73 +1,80 @@
 require('./node_modules/phantomjs-nodify');
 var flow = require('./node_modules/q');
-var brraTab = require("webpage").create();
-var brraFlow;
-var firstUrl = "https://public.brra.bg/CheckUps/Verifications/VerificationPersonOrg.ra";
 
-
-function
-brraTab.onLoadFinished = function (status) {
-    brraFlow.resolve(status);
+function BrraShort(name) {
+    var self = this;
+    this.name = name;
+    this.tab = require("webpage").create();
+    this.tab.onLoadFinished = function (status) { self.flow.resolve(status); }
 }
+BrraShort.prototype = {
+    name: null,
+    tab: null,
+    flow: null,
+    _step: function(logic) {
+        var self = this;
+        return function() {
+            self.flow = flow.defer();
+            logic.apply(this, arguments);
+            return brraFlow.promise;
+        }
+    },
+    run: function() {
+        var self = this;
+        var step = this._step; // shortcut to step func
 
-var step = function(logic) {
-    return function() {
-        brraFlow = flow.defer();
-        logic.apply(this, arguments);
-        return brraFlow.promise;
+        flow("https://public.brra.bg/CheckUps/Verifications/VerificationPersonOrg.ra")
+
+        // Open brra.bg landing page
+        .then(step(function(url) {
+            self.tab.open(url);
+        }))
+
+        // Fetch captcha text
+        .then(function() {
+            var captchaUrl = self.tab.evaluate(function() {
+                return document.querySelector('[src^="Capt"]').src;
+            })
+            return downloadAndSolveCaptcha(captchaUrl)
+        })
+
+        // Submit form
+        .then(step(function(captchaText) {
+            self.tab.evaluate(function() {
+                var ev = document.createEvent("MouseEvents");
+                ev.initEvent("click", true, true);
+                document.querySelector('.search_form').querySelector('input[name*="CaptchaControl"]').value = solution;
+                document.querySelector('.search_form').querySelector('input[name*="OrganizationName"]').value = mpName;
+                document.querySelector('.search_form').querySelector('input[name*="btnSearch"]').dispatchEvent(ev);
+            }, captchaText)
+        }))
+
+        // Scrape entries
+        .then(function() {
+            //@todo brra.emit('searchResults', self.tab) instead of continuing the chain
+            var entries = self.tab.evaluate(function() {
+                var searchWrapper = document.querySelector(".search_results");
+                var results = searchWrapper.querySelectorAll('tr')
+                var entries = []
+                results.forEach(function (row) {
+                    var company = row.querySelector("a");
+                    if (company == null) return;
+                    var title = row.querySelector('td');
+                    entries.push({
+                        "title": title,
+                        "company": company
+                    })
+                });
+            })
+            return entries;
+        })
+        .then(function(entries) {
+            console.log(entries)
+            phantom.exit()
+        })
     }
 }
 
-// Start by passing url
-flow(firstUrl)
-
-// Open brra.bg landing page
-.then(step(function(url) {
-        brraTab.open(url);
-}))
-
-// Fetch captcha text
-.then(function() {
-    var captchaUrl = brraTab.evaluate(function() {
-        return document.querySelector('[src^="Capt"]').src;
-    })
-    return downloadAndSolveCaptcha(captchaUrl)
-})
-
-// Submit form
-.then(step(function(captchaText) {
-    brraTab.evaluate(function() {
-        var ev = document.createEvent("MouseEvents");
-        ev.initEvent("click", true, true);
-        document.querySelector('.search_form').querySelector('input[name*="CaptchaControl"]').value = solution;
-        document.querySelector('.search_form').querySelector('input[name*="OrganizationName"]').value = mpName;
-        document.querySelector('.search_form').querySelector('input[name*="btnSearch"]').dispatchEvent(ev);
-    }, captchaText)
-}))
-
-// Scrape entries
-.then(function() {
-    //@todo brra.emit('searchResults', brraTab) instead of continuing the chain
-    var entries = brraTab.evaluate(function() {
-        var searchWrapper = document.querySelector(".search_results");
-        var results = searchWrapper.querySelectorAll('tr')
-        var entries = []
-        results.forEach(function (row) {
-            var company = row.querySelector("a");
-            if (company == null) return;
-            var title = row.querySelector('td');
-            entries.push({
-                "title": title,
-                "company": company
-            })
-        });
-    })
-    return entries;
-})
-.then(function(entries) {
-    console.log(entries)
-    phantom.exit()
-})
 
 function downloadAndSolveCaptcha(captchaUrl) {
     var captchaReady = flow.defer();
