ff0bf9855ef24fd5f82b9f9f62d10e513cc71e1d
Anton Stoychev <antitoxic@gmail.com>
2013-08-07 22:58:45 +0100

    Path typo

--- common/phantom/node-decaptcha/index.js
+++ common/phantom/node-decaptcha/index.js
@@ -3,7 +3,7 @@ var flow = require('./node_modules/q');
 exports = module.exports = function(captchaUrl) {
     var captchaReady = flow.defer();
     var spawn = require("child_process").spawn
-    var decaptcha = spawn('node', ['decaptcha-phantom-bridge.js', captchaUrl]);
+    var decaptcha = spawn('node', ['bridge.js', captchaUrl]);
     var text = '';
     decaptcha.stdout.on('data', function(data) {
         text = text + data;
