31cfcef73f38fa50ea74a478c3dedb1ee2b2f438
Anton Stoychev <antitoxic@gmail.com>
2013-08-11 21:17:23 +0100

    Added new packages to build script

--- build.sh
+++ build.sh
@@ -3,10 +3,14 @@ BASEDIR=$(cd "$(dirname "$0")"; pwd)
 
 npmToInstall=(
 	$BASEDIR/apps/mp-votes
+	$BASEDIR/apps/brra.bg-inquirer
 	$BASEDIR/common/node/csv-util
 	$BASEDIR/common/node/downloader
 	$BASEDIR/common/node/logger
+	$BASEDIR/common/node/decaptcha
 	$BASEDIR/common/node/spreadsheet2csv-node
+	$BASEDIR/common/phantom/decaptcha-auto-node
+	$BASEDIR/common/phantom/decaptcha-manual
 )
 
 composerToInstall=(
