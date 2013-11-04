9a4459321d3340e965e349839570a162393bea8f
banzsh <sbsavov@gmail.com>
2013-11-04 19:13:57 +0200

    Collation set to UTF8

--- apps/website/core/secret_settings.py
+++ apps/website/core/secret_settings.py
@@ -6,6 +6,11 @@ DATABASES = {
         'PASSWORD': '12qw23we',
         'HOST': '',
         'PORT': '',
+        'TEST_CHARSET': None,
+        'TEST_COLLATION': None,
+        'OPTIONS' : {
+            'init_command': 'SET storage_engine=INNODB,character_set_connection=utf8,collation_connection=utf8_unicode_ci'
+        }
     }
 }
 
