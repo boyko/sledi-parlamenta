357499c87f895ad36bf135a6a9f6e9bd30462bd9
Anton Stoychev <antitoxic@gmail.com>
2013-12-12 20:19:27 +0200

    Enable rate delays for http downloader

--- common/node/downloader/index.js
+++ common/node/downloader/index.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var flow = require('q');
 var request = require('request');
 
 var headers = {
@@ -8,27 +9,40 @@ var headers = {
 	"Cache-Control": "max-age=0"
 }
 
-var Downloader = function(logger) {
+var Downloader = function(logger, rateDelay, maxAttempt) {
 	this.logger = logger;
+	this.lastInQueue = flow();
+	this.rateDelay = typeof rateDelay == 'undefined' ? [1,1] : (Array.isArray(rateDelay) ? rateDelay : [rateDelay, rateDelay]);
+	this.maxAttempt = typeof maxAttempt == 'undefined' ? [1,1] : maxAttempt;
 }
+
 Downloader.prototype = {
 	logger: null,
+    lastInQueue: null,
+    rateDelay: null,
+    maxAttempt: null,
 	get: function(url, callback, attempt) {
 		var self = this;
-		return request({
-			"url": url,
-			"headers": headers
-		}, function (err, res, html) {
-			if (err || res.statusCode != 200) {
-				self.logger.error("Can't retrieve " + url + ", "+res.statusCode + ", err: "+res.statusCode);
-			}
-			if (typeof(callback) == 'function') callback(html);
-		})
-        .on('error', function(err) {
-            self.logger.error("Can't retrieve " + url + ", " + err);
-            if (typeof attempt != 'undefined' && attempt>0) return;
-            self.get(url, callback, 1)
-        })
+        return self.lastInQueue =  self.lastInQueue.then(function() {
+            var done = flow.defer();
+            request({
+                "url": url,
+                "headers": headers
+            }, function (err, res, html) {
+                done.resolve()
+                if (err || res.statusCode != 200) {
+                    self.logger.error("Can't retrieve " + url + ", "+res.statusCode + ", err: "+res.statusCode);
+                }
+                if (typeof(callback) == 'function') callback(html);
+            })
+            .on('error', function(err) {
+                self.logger.error("Can't retrieve " + url + ", " + err);
+                if (typeof attempt != 'undefined' && attempt==self.maxAttempt) return;
+                attempt = typeof attempt == 'undefined' ? 0 : attempt;
+                self.get(url, callback, attempt++)
+            })
+            return done.promise;
+        }).delay(Math.floor(Math.random()*(self.rateDelay[1]-self.rateDelay[0]+1)+self.rateDelay[0]))
 	},
 	save: function(url, destination, callback, attempt) {
         var self = this;
