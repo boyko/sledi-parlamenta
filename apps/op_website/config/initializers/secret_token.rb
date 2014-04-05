efabd4914a5c305a8f0bdfac9a50edf99646a088
jsibelius <c_c@abv.bg>
2014-04-05 16:17:20 +0300

    removed django and added rails

---
+++ apps/op_website/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OpWebsite::Application.config.secret_key_base = '6df3ebd3b63ee1433cbc0c672955bc51fbb64c4bd4e34522a0fb7effd4f4930d39e3c098cc0e43a669ce76d2733df4e042fdd343aa590de40bd9f5523544ab6f'
