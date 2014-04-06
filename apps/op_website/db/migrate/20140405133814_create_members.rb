49526e23d8ceb04e414c4f55f631a80d80e55ffb
jsibelius <c_c@abv.bg>
2014-04-07 01:13:51 +0300

    Updated schema for parties and members.

--- apps/op_website/db/migrate/20140405133814_create_members.rb
+++ apps/op_website/db/migrate/20140405133814_create_members.rb
@@ -2,9 +2,15 @@ class CreateMembers < ActiveRecord::Migration
   def change
     create_table :members do |t|
       t.string :name
+      t.integer :gov_site_id
       t.date :birthday
       t.string :hometown
       t.string :profession
+      t.string :position
+      t.string :elected_from
+      t.string :languages
+      t.string :email
+      t.string :website
       t.text :bio
       t.references :party, index: true
 
--- apps/op_website/db/migrate/20140405134600_create_parties.rb
+++ apps/op_website/db/migrate/20140405134600_create_parties.rb
@@ -2,6 +2,9 @@ class CreateParties < ActiveRecord::Migration
   def change
     create_table :parties do |t|
       t.string :name
+      t.string :abbreviation
+      t.string :info
+      t.string :website
 
       t.timestamps
     end
