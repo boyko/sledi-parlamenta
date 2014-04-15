2ebc9b03bb01eff9e4ec99fdc15a3abfa224f3b4
jsibelius <c_c@abv.bg>
2014-04-15 23:14:38 +0300

    Added find_by_three_names method to Member

--- apps/op_website/app/models/member.rb
+++ apps/op_website/app/models/member.rb
@@ -1,9 +1,15 @@
 class Member < ActiveRecord::Base
   belongs_to :party
   has_many :votes
+  has_many :speeches
 
-  def self.find_by_3_names names
-    names = names.split()
+  def self.find_by_three_names names
+    names = names.split
     Member.where(:first_name => names[0], :sir_name => names[1], :last_name => names[2]).first
+  end
+
+  def self.find_by_two_names names
+    names = names.split.map { |n| n.mb_chars.upcase.to_s }
+    Member.where(:first_name => names[0], :last_name => names[1]).first
   end
 end
--- apps/op_website/lib/tasks/persist.rake
+++ apps/op_website/lib/tasks/persist.rake
@@ -8,7 +8,7 @@ namespace :persist do
   task :svv => :environment do
     File.open(data_path + "mp-votes.json").each do |voting|
       voting = JSON.load voting
-      member = Member.find_by_3_names voting['name']
+      member = Member.find_by_three_names voting['name']
 
       raise "No member with name #{voting['name']} found" if member.nil?
 
