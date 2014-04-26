c8ceea53834c2d18d91798e82995b319e6f4599e
jsibelius <c_c@abv.bg>
2014-04-26 14:33:08 +0300

    Added some methods to 'vote' and 'session' models.

--- apps/op_website/app/models/session.rb
+++ apps/op_website/app/models/session.rb
@@ -1,3 +1,12 @@
 class Session < ActiveRecord::Base
   has_many :votings
+
+  def absent_votes
+    absent = self.votings.order("voted_at").joins(:votes).where("votes.value" => "absent").count
+    votings = self.votings.count
+    absent.to_f/votings
+  end
+
+  #Voting.joins(:votes).where('votes.value' => "absent").group("session_id").count
+
 end
