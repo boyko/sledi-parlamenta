07e68c1c695b7f8310b1378bece6da00ddd14f3d
jsibelius <c_c@abv.bg>
2014-05-07 10:55:09 +0300

    Whitelist params for members' controller.

--- apps/op_website/app/controllers/members_controller.rb
+++ apps/op_website/app/controllers/members_controller.rb
@@ -2,16 +2,22 @@ class MembersController < ApplicationController
   def index
 
     query = Member.all
-    query = query.search(params[:q])
+    query = query.search(member_params[:q])
 
-    ids = params.slice(:party_id, :assembly_id).values.delete_if { |v| v.blank? }.map { |v| v.to_i }
+    ids = member_params.slice(:party_id, :assembly_id).values.delete_if { |v| v.blank? }.map { |v| v.to_i }
     query = query.create_joins ids
-    query = query.order(params[:order])
+    query = query.order(member_params[:order])
 
-    @members = query.paginate(:page => params[:page])
+    @members = query.paginate(:page => member_params[:page])
   end
 
   def show
     @member = Member.find(params[:id])
+  end
+
+  private
+
+  def member_params
+    params.slice(:q, :order, :party_id, :assembly_id, :page)
   end
 end
