211d57706bd146ab30aa80bf21c903be1c3bebf2
jsibelius <c_c@abv.bg>
2014-05-08 12:34:33 +0300

    Styled member's profiles a bit.

--- apps/op_website/app/models/member.rb
+++ apps/op_website/app/models/member.rb
@@ -73,6 +73,7 @@ class Member < ActiveRecord::Base
 
   def age
     dob = self.birthday
+    return nil if dob.nil?
     now = Time.now.utc.to_date
     now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
   end
