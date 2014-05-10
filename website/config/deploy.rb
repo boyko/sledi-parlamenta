64f0322777949281c76e9b15601bf591abb119ed
Dimitar Dimitrov <me@ddimitrov.name>
2014-05-10 17:32:43 +0300

    Install and configure capistrano for deployment

---
+++ website/config/deploy.rb
@@ -0,0 +1,24 @@
+# config valid only for Capistrano 3.1
+lock '3.2.1'
+
+set :application,     'parliament.obshtestvo.bg'
+set :deploy_to,       '/home/openparliament/parliament.obshtestvo.bg'
+set :repo_url,        'https://github.com/obshtestvo/open-parliament.git'
+set :linked_files,    %w(config/database.yml)
+set :linked_dirs,     %w(bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system)
+set :keep_releases,   20
+set :rails_env,       'production'
+set :bundle_bins,     fetch(:bundle_bins, []) + %w(puma pumactl)
+
+namespace :deploy do
+  desc 'Restart the application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      within current_path do
+        execute 'pumactl', '--state', current_path.join('tmp/pids/puma.state'), 'restart'
+      end
+    end
+  end
+
+  after :publishing, :restart
+end
---
+++ website/config/deploy/staging.rb
@@ -0,0 +1,3 @@
+server 'marla.ludost.net', user: 'openparliament', roles: %w(app web db)
+
+set :deploy_to, '/home/openparliament/staging.parliament.obshtestvo.bg'
