4d7ee2be5a7f1585b2e09ebb3b05dfff24799a34
jsibelius <c_c@abv.bg>
2014-05-12 10:23:44 +0300

    Removed columns from 'members' table and added 'constituency' to 'participations' table.

--- website/db/migrate/20140405133814_create_members.rb
+++ website/db/migrate/20140405133814_create_members.rb
@@ -4,21 +4,15 @@ class CreateMembers < ActiveRecord::Migration
       t.string :first_name
       t.string :sir_name
       t.string :last_name
-      t.string :gov_site_id
+      t.string :gov_site_ids
       t.date :birthday
       t.string :hometown
       t.string :profession
       t.string :languages
       t.string :marital_status
-      t.string :constituency
       t.string :email
       t.string :website
-      t.string :position
-      t.boolean :non_affiliated
-      t.string :status
       t.string :gender
-      t.boolean :council_ministers
-      t.string :cm_position
       t.text :bio
 
       t.timestamps
--- website/db/migrate/20140418192605_create_participations.rb
+++ website/db/migrate/20140418192605_create_participations.rb
@@ -4,6 +4,7 @@ class CreateParticipations < ActiveRecord::Migration
       t.references :member, index: true
       t.references :structure, index: true
       t.string :position
+      t.string :constituency
       t.date :start_date
       t.date :end_date
 
