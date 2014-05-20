d302d6501a31e885911e1705705fdb58b3c65375
jsibelius <c_c@abv.bg>
2014-05-20 17:42:04 +0300

    Deleted old downloader and adder new one.

---
+++ data_mining/downloader-mp-votes/downloader.rb
@@ -0,0 +1,18 @@
+require "json"
+
+path = ARGV[0]
+
+STDIN.read.split("\n").each do |line|
+  ob = JSON.parse line
+  date = ob["date"]
+  session_path = path + "/" + date
+  `mkdir -p #{session_path}`
+  names = []
+  ob["xls"].each do |url|
+    names << url.split('/')[-1]
+    `cd #{session_path}; curl -sO #{url}`
+    sleep (rand()*4).round+1
+  end
+  p names
+end
+
