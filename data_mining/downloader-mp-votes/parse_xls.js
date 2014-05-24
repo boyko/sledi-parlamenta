601e18aa10e5d3ebb08792caef297d082e948c87
jsibelius <c_c@abv.bg>
2014-05-24 13:39:41 +0300

    Added mappers to the xls parsing script.

--- data_mining/downloader-mp-votes/parse_xls.js
+++ data_mining/downloader-mp-votes/parse_xls.js
@@ -1,34 +1,36 @@
-// first argument - directory with xlses
-
 var fs = require('fs');
 var XLS = require('xlsjs');
 var LineStream = require('byline').LineStream;
-var parse = require('csv-parse');
+
+var voteMapper = {
+  "+": "yes",
+  "-": "no",
+  "=": "abstain",
+  "0": "absent"
+};
+
+var registrationMapper = {
+  "О": "not_registered",
+  "Р": "manually_registered",
+  "П": "registered"
+};
+
+var markers = {
+  groupVoting: "Регистрации и гласувания по ПГ",
+  topicSplitter: " по тема ",
+  timeRegex: /\d{2}:\d{2}/,
+  dateRegex: /\d{2}-\d{2}-\d{4}/
+};
 
 // helper functions
-function isIndividualVoting(xls) {
-  return xls.SheetNames.indexOf("Регистрации и гласувания по ПГ") === -1;
+function isGroupVoting(xls) {
+  return xls.SheetNames.indexOf(markers.groupVoting) > -1;
 }
 
-function splitter(sheet) {
-  var output = [];
-  var parser = parse({delimiter: ','})
-  parser.on('readable', function(){
-    while(row = parser.read()){
-      output.push(row)
-    }
-  });
-  parser.on('error', function(err){
-    // TODO implement with logger
-  });
-  parser.write(XLS.utils.sheet_to_csv(sheet));
-  parser.end();
-
-  return output;
-}
-
-function isNumber(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
+// transform the csv to a matrix
+function prepare(sheet) {
+  return XLS.utils.sheet_to_csv(sheet).split("\n")
+         .map(function(row) { return row.split(",") });
 }
 
 function getFirstSheet(xls) {
@@ -41,59 +43,66 @@ function column(matrix, idx) {
 }
 
 // main functions
-function extractVotes(sheet, votings_count) {
-  var votes = splitter(sheet);
-  return votes
-    .filter(function(row) { return isNumber(row[2]) })
-    .map(function(row) {
-      return row.filter(function(cell, idx) {
-        return ["+", "-", "=", "0", "П", "О", "Р"].indexOf(cell) !== -1 || idx === 0;
-      });
-    });
-}
+function extractVotesOrRegistrations(votesData, mapObject) {
+  return votesData.map(function(row) {
+    // filter cells that are not votings/registrations values
+    return row.filter(function(cell) { return Object.keys(mapObject).indexOf(cell) > -1; })
+              // remap cells to human readable values
+              .map(function(cell) { return mapObject[cell] });
+  });
+};
 
-function extractVotings(sheet) {
-  var votings = splitter(sheet)
-    .filter(function(row) { return /ГЛАСУВАНЕ/.test(row[0]) } )
-    .map(function(row) {
-      var res = row[0].match(/(\d+:\d+) по тема (.+)/);
-      return [res[1], res[2]];
+function extractMembers(votesData) {
+  return votesData.map(function(row) { return row[0] });
+};
+
+function extractVotings(votingsData) {
+  return votingsData
+    .filter(function(row) { return /ГЛАСУВАНЕ/.test(row[0]); } )
+    .map(function(voting_name_row) {
+      var time = voting_name_row[0].match(markers.timeRegex)[0];
+      var topic = voting_name_row[0].split(markers.topicSplitter)[1];
+      return [time, topic];
     });
-  var registration = sheet['A2'].v.match(/(\d+-\d+-\d+) (\d+:\d+)/);
-  return [registration[1], [registration[2], "Регистрация"]].concat(votings);
-}
+};
 
 function parseData(iv_sheet, gv_sheet) {
-  var votings = extractVotings(gv_sheet),
-      votes = extractVotes(iv_sheet),
+  var votingsData = prepare(gv_sheet),
+      votesData = prepare(iv_sheet)
+        // fetch only rows with the members and corresponding votings.
+        // The rows are filtered by the data in the registration cell - it should be only О, П and Р. (cyrillic)
+        .filter(function(row) { return Object.keys(registrationMapper).indexOf(row[4]) > -1 }),
+      votings = extractVotings(votingsData),
+      votes = extractVotesOrRegistrations(votesData, voteMapper),
       session = {
-        date: votings.shift(),
-        members: column(votes, 0)
+        date: gv_sheet['A2'].v.match(markers.dateRegex)[0],
+        members: extractMembers(votesData),
+        registration: {
+          time: gv_sheet['A2'].v.match(markers.timeRegex)[0],
+          data: column(extractVotesOrRegistrations(votesData, registrationMapper), 0)
+        }
       };
 
-  votings = votings.map(function(voting, idx) {
+  session.votings = votings.map(function(voting, idx) {
     return {
       time: voting[0],
       topic: voting[1],
-      votes: column(votes, idx+1)
+      votes: column(votes, idx)
     }
   });
 
-  session.votings = votings;
-
   console.log(JSON.stringify(session));
-}
+};
 
 function init(paths) {
-  var xls = [XLS.readFile(paths[0]), XLS.readFile(paths[1])]
+  var xls = [XLS.readFile(paths[0]), XLS.readFile(paths[1])];
 
-  if(!isIndividualVoting(xls[0]))
-    xls.reverse()
+  if(isGroupVoting(xls[0]))
+    xls.reverse();
 
   parseData(getFirstSheet(xls[0]), getFirstSheet(xls[1]));
-}
+};
 
-//process.stdin.resume();
 process.stdin.pipe(new LineStream()).on('data', function(line) {
-  init(JSON.parse(line.toString()))
-})
+  init(JSON.parse(line.toString()));
+});
