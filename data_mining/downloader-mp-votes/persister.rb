309a774003e9390a1b5881aa6bb749a8b1e9503d
jsibelius <c_c@abv.bg>
2014-05-26 23:40:35 +0300

    Added assignment for votings.voted_at and sessions.assembly.

--- data_mining/downloader-mp-votes/persister.rb
+++ data_mining/downloader-mp-votes/persister.rb
@@ -4,13 +4,21 @@ require 'json'
 $stdin.each_line do |session_str|
   session = JSON.parse session_str
 
+  members = session['members'].map do |m|
+    names = m.mb_chars.titleize.to_s
+    Member.find_by_three_names names
+  end
+
+  date = session['date']
+
   # We must use find_or_create_by, because there is another script
   # that saves the stenograph and url and we don't know the which
   # one is executed first.
-  s = Session.find_or_create_by(date: session['date'])
+  s = Session.find_or_create_by(date: date)
 
   session['votings'].each do |voting|
-    v = Voting.create(session: s, topic: voting['topic'])
+    time = date + " " + voting['time']
+    v = Voting.create(session: s, topic: voting['topic'], voted_at: time)
 
     voting['votes'].each_with_index do |vote, idx|
       Vote.create(member: members[idx], voting: v, value: Vote.values[vote.to_sym])
--- data_mining/downloader-mp-votes/persister_stenographs.rb
+++ data_mining/downloader-mp-votes/persister_stenographs.rb
@@ -4,12 +4,15 @@ require 'json'
 $stdin.each_line do |session_str|
   session = JSON.parse session_str
 
-  
+  date = session['date']
+
+  a = Structure.assemblies.by_date(date)
 
   # We must use find_or_create_by, because there is another script
   # that saves the stenograph and url and we don't know the which
   # one is executed first.
-  s = Session.find_or_create_by(date: session['date'])
+  s = Session.find_or_create_by(date: date)
+  s.assembly = a
   s.stenograph = session['stenograph']
   s.url = session['url']
   s.save
