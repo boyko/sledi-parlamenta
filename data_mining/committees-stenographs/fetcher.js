78da1be5ac81ae73d1bae48a0e1885195547024e
jsibelius <c_c@abv.bg>
2014-06-04 11:22:36 +0300

    Added fetcher for committee stenographs.

---
+++ data_mining/committees-stenographs/fetcher.js
@@ -0,0 +1,58 @@
+var urlInfo = require('url')
+var Downloader = require('../common/node/downloader')
+var InputManager = require('./input')
+var $ = require('cheerio');
+
+// input handling
+var inputMan = new InputManager();
+var argv = inputMan.getArguments();
+
+var logger = require('../common/node/logger')(inputMan.retrieveLoggerConfig(argv))
+var downloader =  new Downloader(logger, [1000, 5000]);
+
+function getFullUrl(el) {
+  return "http://www.parliament.bg" + $(el).attr('href');
+}
+
+function scrapeSession(url, html) {
+  var date = $(".dateclass", html).text().trim()
+             .replace(/(\d{2})\/(\d{2})\/(\d{4})/g, function(match, d, m, y) { return [y, m, d].join("-") });
+  var stenograph = $("#leftcontent .markcontent", html).text();
+  var name = $("#leftcontent .marktitle", html).contents().eq(0).text();
+
+  console.log(JSON.stringify({
+    name: name,
+    date: date,
+    url: url,
+    stenograph: stenograph
+  }));
+}
+
+function scrapeSessions(html) {
+  $("#monthview a", html).each(function(i, el) {
+    var url = getFullUrl(el);
+    downloader.get(url, function(html) {
+      scrapeSession(url, html)
+    });
+  });
+}
+
+function scrapeMonths(html) {
+  $("#calendar a", html).each(function(i, el) {
+    var url = getFullUrl(el);
+    downloader.get(url, scrapeSessions);
+  });
+}
+
+function scrapeCommittees(html) {
+  $(".DataBlock a", html).each(function(i, el) {
+    var url = getFullUrl(el) + "/steno";
+    downloader.get(url, scrapeMonths);
+  });
+}
+
+function init() {
+  downloader.get(argv.url, scrapeCommittees);
+}
+
+init();
