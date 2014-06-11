8af7eb77fc29f19c6038a29a7998c61e3cf958c3
Dimitar Dimitrov <me@ddimitrov.name>
2014-06-11 22:49:30 +0300

    Use the capistrano3-puma gem for managing puma

--- website/config/deploy.rb
+++ website/config/deploy.rb
@@ -9,22 +9,8 @@ set :linked_files,    %w(config/database.yml)
 set :linked_dirs,     %w(bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system)
 set :keep_releases,   20
 set :rails_env,       'production'
-set :bundle_bins,     fetch(:bundle_bins, []) + %w(puma pumactl)
 set :git_strategy,    Capistrano::GitSubfolderStrategy
 set :git_subfolder,   '/website'
-
-namespace :deploy do
-  desc 'Restart the application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      within current_path do
-        puma_state_file = current_path.join('tmp/pids/puma.state')
-        if test :test, '-f', puma_state_file
-          execute :pumactl, '--state', puma_state_file, 'restart'
-        end
-      end
-    end
-  end
-
-  after :publishing, :restart
-end
+set :puma_threads,    [15, 15]
+set :puma_workers,    3
+set :puma_init_active_record, true
