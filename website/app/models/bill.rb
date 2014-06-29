499485cc8295b303fb7b167416990af366d0a0bf
jsibelius <c_c@abv.bg>
2014-06-29 19:07:13 +0300

    Added statuses relation to 'bill' model.

--- website/app/models/bill.rb
+++ website/app/models/bill.rb
@@ -2,4 +2,6 @@ class Bill < ActiveRecord::Base
   has_and_belongs_to_many :members
   has_many :reviews
   has_many :committees, through: :reviews, foreign_key: :structure_id, source: :structure
+
+  has_many :statuses
 end
