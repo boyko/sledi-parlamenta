90e7f994f7046aca3f85bbb1bc00d24f28c5e866
jsibelius <c_c@abv.bg>
2014-08-10 02:08:39 +0300

    Whitelisted params for session controller.

--- website/app/controllers/sessions_controller.rb
+++ website/app/controllers/sessions_controller.rb
@@ -1,11 +1,11 @@
 class SessionsController < ApplicationController
   def index
-    @year = params[:year].blank? ? Date.today : Date.new(params[:year].to_i)
+    @year = sessions_params[:year].blank? ? Date.today : Date.new(sessions_params[:year].to_i)
     @sessions = Session.assemblies.by_year(@year).group_by { |s| s.date }
   end
 
   def show
-    @session = Session.find(params[:id])
+    @session = Session.find(sessions_params[:id])
     @votings = @session.votings
     if @votings.count == 0
       flash.now[:notice] = "Не бяха намерени гласувания за тази сесия."
@@ -13,24 +13,31 @@ class SessionsController < ApplicationController
   end
 
   def attendance
-    data = Session.find(params[:session_id]).absent_count_by_voting
+    data = Session.find(sessions_params[:session_id]).absent_count_by_voting
     render :json => data
   end
 
   def votings
-    s = Session.find(params[:session_id])
+    s = Session.find(sessions_params[:session_id])
     render :json => s.votings.ordered.pluck(:id, :topic, :voted_at)
   end
 
   def prev
-    current_session = Session.find(params[:session_id])
+    current_session = Session.find(sessions_params[:session_id])
     redirect_to session_url(current_session.prev)
   end
 
   def next
-    current_session = Session.find(params[:session_id])
+    current_session = Session.find(sessions_params[:session_id])
     redirect_to session_url(current_session.next)
   end
 
+
+
 end
 
+private
+
+  def sessions_params
+    params.slice(:id, :session_id, :year)
+  end
