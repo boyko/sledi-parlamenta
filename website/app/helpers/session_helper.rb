5ac59c2ca7df597827ff9d3a4072b3745eee1482
jsibelius <c_c@abv.bg>
2014-08-10 23:31:04 +0300

    Cleaned session helper and model from deprecated code.

--- website/app/helpers/session_helper.rb
+++ website/app/helpers/session_helper.rb
@@ -3,74 +3,11 @@ module SessionHelper
   # s is for session. 'session' is reserved word for rails
   def process_stenograph s
 
-    members = s.members.pluck(:id, :first_name, :last_name).map { |m| [m[0], m[1] + " " + m[2]] }
-    votings = s.votes_by_voting.keys.group_by { |v| v[0] }.values
-
     stenograph = "<pre>" + s.stenograph
-
-    stenograph.gsub! "\r\n", "<br>"
-
-    members.each do |m|
-      regex = /(#{m[1]}|#{m[1].mb_chars.upcase.to_s})/
-      stenograph.gsub! regex, "<a href='/members/#{m[0]}'>#{m[1]}</a>"
-    end
-
-    if votings.length > 0
-
-      regex = /Гласували \d+ народни представители: за (\d+|няма), против (\d+,|няма,|и) въздържал(и се| се) (– )?(\d+|няма)\./
-      regex_enum = stenograph.gsub! regex
-
-      regex_enum.each_with_index do |match, idx|
-        match + svg_diagram(votings[idx])
-      end
-    end
-
     stenograph + "</pre>"
   end
 
   private
-
-  def svg_diagram voting
-
-    return "" if voting.nil?
-
-    by_parties = voting.group_by { |v| v[1] }
-    data = by_parties.map do |k, v|
-      {
-        party: k,
-        members: v.map { |i|
-          {
-            id: i[2],
-            name: i[3] + " " + i[4],
-            vote: i[5]
-          }
-        }
-      }
-    end
-
-    cols = 8
-    width = 15
-    height = 15
-    margin = 1
-    gr_margin = 20
-
-    width_svg = (width+margin)*cols*data.length
-
-    html = "<br><svg width='#{width_svg}' height='220'>"
-    data.each_with_index do |p, outer_idx|
-      offset = outer_idx * (cols * (width + margin) + gr_margin)
-      p[:members].each_with_index do |el, idx|
-        x = (idx % cols) * (width + margin) + offset
-        y = (idx / cols) * (width + margin)
-        html += "<a xlink:href='/members/#{el[:id]}'><g><rect width='#{width}' height='#{height}' x='#{x}' y='#{y}' class='#{Vote.values.key(el[:vote]).to_s}' data-toggle='tooltip' data-placement='top' title='#{el[:name]}'></g></a>"
-      end
-      html += "<text x='#{offset+(outer_idx*10)}' y='210' font-family='Verdana' font-size='10'>#{p[:party]}</text>"
-    end
-
-    html += "</svg>"
-    html.html_safe
-
-  end
 
   def calendar year, &block
     year = (year.beginning_of_year..year.end_of_year).map { |d| Date.new(d.year, d.month, 1) }.uniq.in_groups_of(4)
--- website/app/models/session.rb
+++ website/app/models/session.rb
@@ -27,34 +27,4 @@ class Session < ActiveRecord::Base
     Session.by_structure_name(structure_name).where(sess[:date].gt(self.date)).order("date asc").first
   end
 
-  def absent_votes
-    absent = self.votings.order("voted_at").joins(:votes).where("votes.value" => "absent").count
-    votings = self.votings.count
-    absent.to_f/votings
-  end
-
-  def absent
-    self.votes.absent
-  end
-
-  def absent_count
-    self.absent.count
-  end
-
-  def absent_count_by_voting
-    data = self.absent.joins(:voting).group("votings.voted_at").count
-    data.map { |d| [d[0].to_datetime.strftime('%H:%M'), d[1]] }
-  end
-
-  def votes_by_voting
-    participation = Participation.arel_table
-    date = self.date
-
-    self.votes.joins({ member: :structures }).order("votings.voted_at")
-    .where((participation[:start_date].lt(date)).and(participation[:end_date].gt(date)).or(
-           (participation[:start_date].lt(date)).and(participation[:end_date].eq(nil))))
-    .where(structures: { kind: Structure.kinds[:party]})
-    .group("votings.voted_at", "structures.name", "members.id", "members.first_name", "members.last_name", "value").count
-  end
-
 end
