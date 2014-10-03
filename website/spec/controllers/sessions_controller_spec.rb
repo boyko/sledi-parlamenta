9d9e17b20f6b773b76be7f6d0edac215f4044511
Stefan Adamov <stefan.adamof@gmail.com>
2014-10-03 16:12:12 +0300

    Adds rspec

---
+++ website/spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe SessionsController do
+  fixtures :sessions
+
+  describe 'index' do
+    it 'should render OK' do
+      get :index
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'show' do
+    it 'should render OK' do
+      get :show, id: Session.last
+      expect(response).to be_successful
+    end
+  end
+end
