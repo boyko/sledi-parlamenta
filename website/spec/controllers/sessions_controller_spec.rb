9bf8480bdcd7f5276035d263eb9a3ada3272aafe
Stefan Adamov <stefan.adamof@gmail.com>
2014-10-04 15:56:18 +0300

    Small spec refactor

--- website/spec/controllers/sessions_controller_spec.rb
+++ website/spec/controllers/sessions_controller_spec.rb
@@ -20,9 +20,7 @@ describe SessionsController do
       it 'should only show sessions from that year' do
         get :index, year: '2013'
 
-        expect(assigns[:sessions]).to eql(
-          session.date => [session]
-        )
+        expect(assigns[:sessions].values.flatten).to eql([session])
       end
     end
 
@@ -30,10 +28,8 @@ describe SessionsController do
       it 'should only show sessions from the current year' do
         get :index
 
-        expect(assigns[:sessions]).to eql(
-          sessions(:new_1).date => [sessions(:new_1)],
-          sessions(:new_2).date => [sessions(:new_2)],
-          sessions(:new_3).date => [sessions(:new_3)]
+        expect(assigns[:sessions].values.flatten).to eql(
+          [sessions(:new_1), sessions(:new_2), sessions(:new_3)]
         )
       end
     end
@@ -46,9 +42,8 @@ describe SessionsController do
           matched_sesion_1 = sessions(:new_2)
           matched_sesion_2 = sessions(:new_3)
 
-          expect(assigns[:sessions]).to eql(
-            matched_sesion_1.date => [matched_sesion_1],
-            matched_sesion_2.date => [matched_sesion_2]
+          expect(assigns[:sessions].values.flatten).to eql(
+            [matched_sesion_1, matched_sesion_2]
           )
         end
       end
@@ -57,9 +52,7 @@ describe SessionsController do
         it 'retrieves the matching sessions from the given year' do
           get :index, query: 'братле', year: '2013'
 
-          expect(assigns[:sessions]).to eql(
-            sessions(:old).date => [sessions(:old)]
-          )
+          expect(assigns[:sessions].values.flatten).to eql([sessions(:old)])
         end
       end
     end
